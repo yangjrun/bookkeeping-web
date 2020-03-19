b0e510c353a1ca14636895ece223e391403ac6e6
yangjrun <31336383+yangjrun@users.noreply.github.com>
2020-03-19 09:50:56 +0800

    UPDATE  修改启动配置

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: '149.129.47.81 ', // can be overwritten by process.env.HOST
+    port: 30001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
